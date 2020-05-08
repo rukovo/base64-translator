cdb2b418497d25a96539f4b9f0d90e1cb535b9df
rukovo <53445606+rukovo@users.noreply.github.com>
2020-05-08 03:10:51 -0400

    Add files via upload

--- base64dencoder-clean.py
+++ base64dencoder-clean.py
@@ -1,30 +1,51 @@
+#! /usr/bin/env python3
+
 import base64
 import pyperclip
 import time
 
 def print_copy_ask():
-    print(s2)
     pyperclip.copy(s2)
-    print("Would you like to do more? (y/n)")
-    answer = input('> ')
+    print("Here's your data: \n" + s2)
+    print()
+    answer = input('Would you like to do more? (y/n) \n > ')
     return answer
+
+
+def bencode():
+    user_data = input('Enter your data to encode: \n > ')
+    s2 = str(base64.b64encode(user_data.encode('ascii')))
+    s2 = s2[2:-1]
+    return s2
+
+
+def bedcode():
+    user_data = input('Enter your data to decode: \n > ')
+    s2 = base64.b64decode(user_data).decode('ascii')
+    return s2
+
+
+def beddcode():
+    user_data = input('Enter your data to decode: \n > ')
+    s2 = base64.b64decode(user_data).decode('ascii')
+    s2 = base64.b64decode(s2).decode('ascii')
+    return s2
 
 
 answer = 'y'
 while answer.lower() == "y":
-    print('Would you like to encode or decode? (e/d)')
-    code = input('> ')
+    code = input('Would you like to encode or decode? (e/d) \n > ')
     if code.lower() == 'e':
-        print('Enter your data to encode:')
-        user_data = input('> ')
-        s2 = str(base64.b64encode(user_data.encode('ascii')))
-        s2 = s2[2:-1]
+        s2 = bencode()
+        answer = print_copy_ask()
+    elif code.lower() == 'd':
+        s2 = bedcode()
+        answer = print_copy_ask()
+    elif code.lower() == 'dd':
+        s2 = beddcode()
         answer = print_copy_ask()
     else:
-        print('Enter your data to decode:')
-        user_data = input('> ')
-        s2 = base64.b64decode(user_data).decode('ascii')
-        answer = print_copy_ask()
+        print('Please correctly choose one of the options. \n Try again. \n 🤡')
 else:
     print("Goodbye")
     time.sleep(.5)

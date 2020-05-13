76d8f4b2ec1a5a8d32a3a85c29c390c74da3146b
rukovo <53445606+rukovo@users.noreply.github.com>
2020-05-12 23:12:26 -0400

    Lots of changes.
    Changed y/n menus with adding options via a "|" followed by dashes "-" with your options following the dashes.
    Ex. "This is my test to encode. | -e"
    Also lets you save your encoded/decoded messages to a text file.

--- base64dencoder.py
+++ base64dencoder.py
@@ -3,49 +3,49 @@
 import base64
 import pyperclip
 import time
+import os
 
-def print_copy_ask():
-    pyperclip.copy(s2)
-    print("Here's your fuckin shit: \n" + s2)
-    print()
-    answer = input('Would you like to do more? (y/n) \n > ')
-    return answer
-
-
-def bencode():
-    shit = input('Enter your data to encode: \n > ')
-    s2 = str(base64.b64encode(shit.encode('ascii')))
-    s2 = s2[2:-1]
-    return s2
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+link_list = 'link_list.txt'
+arguments = {'decode': '-d',
+             'encode': '-e', 'save new': '-sn',
+             'save append': '-sa',
+             'decode twice': '-dd'}
 
 
-def bedcode():
-    shit = input('Enter your fuckin shit: \n > ')
-    s2 = base64.b64decode(shit).decode('ascii')
-    return s2
-
-
-def beddcode():
-    shit = input('Enter your fuckin shit: \n > ')
-    s2 = base64.b64decode(shit).decode('ascii')
-    s2 = base64.b64decode(s2).decode('ascii')
-    return s2
+def list_title(width):
+    link_title = input('What do you want the title to be?\n > ')
+    link_title = link_title.title()
+    title_title = link_title.center(width, '-')
+    return title_title
 
 
 answer = 'y'
 while answer.lower() == "y":
-    code = input('Would you like to encode or decode? (e/d) \n > ')
-    if code.lower() == 'e':
-        s2 = bencode()
-        answer = print_copy_ask()
-    elif code.lower() == 'd':
-        s2 = bedcode()
-        answer = print_copy_ask()
-    elif code.lower() == 'dd':
-        s2 = beddcode()
-        answer = print_copy_ask()
-    else:
-        print('Enter the right shit dumbass 🤡 \n Try again.')
+    user_input = input('Enter your data')
+    user_list = user_input.split('|')
+    user_data = user_list[0].strip()
+    user_args = user_list[1].replace(' ', '')
+    user_args = user_args.split('-')
+    if 'e' in user_args:
+        s2 = str(base64.b64encode(user_data.encode('ascii')))
+        s2 = s2[2:-1]
+    if 'd' in user_args:
+        s2 = base64.b64decode(user_data).decode('ascii')
+    if 'sn' in user_args:
+        open_list = open(link_list, 'a')
+        title_title = list_title(40)
+        open_list.write('\n' + title_title + '\n' + s2 + '\n')
+        open_list.close()
+    if 'sa' in user_args:
+        open_list = open(link_list, 'a')
+        open_list.write(s2 + '\n')
+        open_list.close()
+    pyperclip.copy(s2)
+    print(f"\n Here's your data:\n{s2}\n")
+    answer = input('Would you like to do more? (y/n) \n > ')
 else:
     print("Goodbye")
     time.sleep(.5)
